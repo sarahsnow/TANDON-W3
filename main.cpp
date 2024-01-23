ff8d12afd56115040ed1c54c1e5fc2c82d048ba8
Snow <69989204+sarahsnow@users.noreply.github.com>
2024-01-23 01:13:31 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+    int hour, mins;
+    string day;
+    string weekDay, weekend;
+    double cost;
+
+    double callLength;
+
+
+   cout << "please enter the time in the 24 hour format with time and minutes seperated by a space" << endl;
+   cin >> hour >> mins;
+
+   cout << "please enter the date in the format (Mo Tu We Th Fr Sa Su) " << endl;
+   cin >> day;
+
+   cout << "please enter how many minutes the call was " << endl;
+   cin >> callLength;
+
+   if ( day == "Mo" || day == "Tu" || day == "We" || day == "Th" || day == "Fr") {
+       weekDay = day ;
+   }
+   else  {
+      weekend = day;
+   }
+
+
+   if (day == weekDay) {
+       if (hour >= 8 && hour <=18){
+           cost = callLength * 0.4;
+       }
+       else {
+           cost = callLength * 0.25;
+       }
+   }
+
+   else {
+       cost = callLength * 0.15;
+
+   }
+
+    cout << "your call costs $" << cost << endl;
+
+
+
+
+
+
+    return 0;
+}
